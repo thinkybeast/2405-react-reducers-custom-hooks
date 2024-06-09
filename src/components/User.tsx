b133f5b3b3ca889bc4899b76e11957eb50d69a1c
Max Hawkins <max.hawkins@hey.com>
2024-06-09 14:32:24 -0700

    7 - Reimplement useState with useReducer

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,6 +4,24 @@ import Loading from "./Loading";
 import UserError from "./UserError";
 import { randomErrorString } from "@/utils";
 
+/*
+ Key idea: 
+ 1. Use reducers when you have complex, interdependent state
+ 2. Where you want to move control of the changes in state from the component to a "state change" function
+*/
+
+/*
+  Key idea: useReducer is like useState...
+  *except*
+  ...the argument to the setter function is not the next state,
+  ...the argument to the setter function is the argument to the reducer function
+*/
+
+/*
+  Key idea: 
+  The return value of the reducer function will be your next state
+*/
+
 const userSchema = z.object({
   avatar: z.string(),
   first_name: z.string(),
@@ -14,17 +32,35 @@ const userSchema = z.object({
 
 type User = z.infer<typeof userSchema>;
 
+// Define the initial state of the component data
+type UserState = {
+  user: User | null;
+  isLoading: boolean;
+  error: boolean;
+};
+
+const initialUserState: UserState = {
+  user: null,
+  isLoading: false,
+  error: false,
+};
+
+// Define the reducer function. It takes the current state and an action, and returns the next state
+const userReducer = (_currentState: UserState, arg: UserState) => {
+  return arg;
+};
+
 const User = () => {
-  const [user, setUser] = React.useState<User | null>(null);
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<boolean>(false);
+  const [userState, setUserState] = React.useReducer(
+    userReducer,
+    initialUserState
+  );
+  const { user, isLoading, error } = userState; // Destructure the state object
 
   async function fetchUser() {
     try {
-      // Reset state
-      setIsLoading(true);
-      setUser(null);
-      setError(false);
+      // Set Loading state
+      setUserState({ isLoading: true, error: false, user: null });
 
       // Simulate a delay
       await new Promise<void>((resolve) => setTimeout(() => resolve(), 1500));
@@ -38,14 +74,14 @@ const User = () => {
 
       // Validate API response
       userSchema.parse(userData);
-      setUser(userData);
+
+      // Set user state
+      setUserState({ user: userData, isLoading: false, error: false });
     } catch (error) {
       // Set error state and log error
-      setError(true);
+      setUserState({ user: null, isLoading: false, error: true });
+
       console.error(error);
-    } finally {
-      // Reset loading state
-      setIsLoading(false);
     }
   }
 
