69bb027a7ab47fa115ef9a5992c3de3567cd5485
Max Hawkins <max.hawkins@hey.com>
2024-06-09 12:41:07 -0700

    2 - Add Refetch button

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -14,24 +14,25 @@ type User = z.infer<typeof userSchema>;
 const User = () => {
   const [user, setUser] = React.useState<User | null>(null);
 
-  React.useEffect(() => {
-    async function fetchUser() {
-      try {
-        const result = await fetch("https://random-data-api.com/api/v2/users");
-        const userData = await result.json();
-        userSchema.parse(userData);
-        setUser(userData);
-      } catch (error) {
-        console.error(error);
-      }
+  async function fetchUser() {
+    try {
+      const result = await fetch("https://random-data-api.com/api/v2/users");
+      const userData = await result.json();
+      userSchema.parse(userData);
+      setUser(userData);
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  React.useEffect(() => {
     fetchUser();
   }, []);
 
   return (
     <article>
       {user ? (
-        <>
+        <div>
           <div style={{ width: "310px", height: "310px", margin: "0 auto" }}>
             <img src={user.avatar} />
           </div>
@@ -39,7 +40,8 @@ const User = () => {
             Meet <b>{user.first_name}!</b>
           </p>
           <p>They are passionate about {user.employment.key_skill}</p>
-        </>
+          <button onClick={fetchUser}>Not cool enough. Give me another.</button>
+        </div>
       ) : null}
     </article>
   );
